dd52dca64b26adc1c3e7924e86848792774972fb
gulu <1105026730@qq.com>
2026-08-18 00:57:20 +0800

    docs(python-basic): 优化迭代器惰性遍历示例

--- src/python_basic/8_迭代器/2_手写迭代器.py
+++ src/python_basic/8_迭代器/2_手写迭代器.py
@@ -9,7 +9,7 @@ class Person:
         self.weight = weight
 
     def __iter__(self):
-        return iter([self.name, self.age, self.height, self.weight])
+        return iter([self.name, self.age, self.height, self.weight])  # 直接返回全量数据的迭代器
 
 
 person = Person("zhangsan", 19, 181, 80.0)
@@ -17,7 +17,7 @@ for p in iter(person):
     print(p)
 
 
-# yield作用：产出一个值 + 暂停当前位置 + 下次从这里继续。
+# yield作用：产出一个值 + 暂停当前位置 + 下次从这里继续。（惰性遍历）
 class Person2:
     def __init__(self, name, age, height, weight):
         self.name = name
@@ -34,10 +34,11 @@ class Person2:
 
 person2 = Person2("zhangsan", 19, 181, 80.0)
 for p in person2.get_fields():
-    print(p)
+    print(p)  # 遍历到哪条数据，哪条数据才会被加载进内存
 
 
-# yield from适合组合加工，分段产生数据
+# yield from适合组合加工，将分段产生的数据合并为一个新的生成器
+# （比直接iter()的优势是惰性遍历，只有迭代到的分段数据才会加载进内存）
 class Person3:
     def __init__(self, name, age, height, weight):
         self.name = name
@@ -46,8 +47,8 @@ class Person3:
         self.weight = weight
 
     def get_fields(self):
-        yield from iter(("第一段", self.name, self.age))
-        yield from iter(("第二段", self.height, self.weight))
+        yield from ("第一段", self.name, self.age)
+        yield from ("第二段", self.height, self.weight)
 
 
 person3 = Person3("33", 33, 33, 33.0)
